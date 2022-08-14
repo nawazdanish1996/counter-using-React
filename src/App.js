c1c3c7b00f3383e9bc3a0f2397194d290ac3bbc0
Nawaz Danish <nawazdanish1996@gmail.com>
2022-08-14 15:44:26 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+      <Counter title="Counter buttons(Increment, Decrement, & Reset) in React" />
+  );
+}
+
+export default App;
---
+++ src/components/Counter.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "../components/Counter.css";
+
+const Counter = (props) => {
+
+    const [count, setCount] = useState(0);
+
+    return(
+        <div className="main-div">
+            <h1>{props.title}</h1>
+            <h2>{count}</h2><br />
+
+            <button
+            onClick={
+                function inc(){
+                    if(count < 10){
+                        setCount(count + 1)
+                    }
+                }
+            }
+            >
+            Increment
+            </button>
+
+            <button
+            onClick={
+                function inc(){
+                    if(count > 0){
+                        setCount(count - 1)
+                    }
+                }
+            }
+            >
+            Decrement
+            </button>
+
+            <button
+            onClick={
+                function res(){
+                    setCount(count - count)
+                }
+            }
+            >
+            Reset
+            </button>
+
+            <div className="para">
+                <p>Increment: Highest order is 10</p>
+                <p>Decrement: below 0 will not be decrement like -1</p>
+                <p>Reset: After click on reset button will be reset the value</p>
+            </div>
+        </div>
+    )
+}
+
+
+export default Counter;
